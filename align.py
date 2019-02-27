21dd4d146920995cb1e9783a1fc1cba1e5cc9998
Remi <r.f.niel@student.rug.nl>
2019-02-27 11:38:53 +0100

    added radius fitting, so only a circle with radius close to that of the mean is accepted

--- align.py
+++ align.py
@@ -1,107 +1,88 @@
 import cv2
 import glob
 import numpy as np
+import progressbar
 
-PADDING = 200;
+PADDING = 100;
  
+imgs = glob.glob('imgs/' +'*.tif',recursive=True)
+imgs.sort()
 
-
-def getBest(img, template, y, x, radius, search_size = 10):
-	# cv2.imshow('detected circles',template)
-	# cv2.waitKey(0)
-
-	res = cv2.matchTemplate(img, template, cv2.TM_CCOEFF_NORMED)
-	yT = y + PADDING - radius
-	xT = x + PADDING - radius
-	min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res[yT - search_size : yT + search_size, xT - search_size : xT + search_size])
-
-	return y - (max_loc[1] - search_size), x - (max_loc[0] - search_size);
-
-
-
-
-imgs = glob.glob('imgs/' +'*.png',recursive=True)
-imgs.sort();
-print(imgs)
-
-stored_imgs = [];
-adjusted_imgs = [];
 stored_circles = [];
-max_radius = 0;
+max_radius = 0
+mean_radius = 0
 
-for idx, file in enumerate(imgs):
-	raw_img = cv2.imread(file,cv2.IMREAD_COLOR)
-	border=cv2.copyMakeBorder(raw_img, top=PADDING, bottom=PADDING, left=PADDING, right=PADDING, borderType= cv2.BORDER_CONSTANT, value=[0,0,0] )
-	stored_imgs.append(border)
+with progressbar.ProgressBar(max_value=len(imgs)) as bar:
+	for idx, file in enumerate(imgs):
+		# if idx > 1:
+		# 	break;
+		bar.update(idx);
+		raw_img = cv2.imread(file,cv2.IMREAD_COLOR)
 
-	gray_img = cv2.imread(file,0)
+		img = cv2.imread(file,0)
 
-	img = cv2.medianBlur(gray_img,5)
-	cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
+		#img = cv2.medianBlur(gray_img,5)
+		cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
 
-	threshold = 100;
-	 
-	circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,20,
-	                            param1=threshold,param2=40,minRadius=0,maxRadius=0)
-
-	while circles is None:
-		threshold -= 1;
+		threshold = 70;
+		min_rad = 0;
+		max_rad = 0;
+		if mean_radius:
+			min_rad = mean_radius - 5;
+			max_rad = mean_radius + 5;
+		 
 		circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,20,
-		                            param1=threshold,param2=40,minRadius=0,maxRadius=0)	
+		                            param1=threshold,param2=40,minRadius=int(min_rad),maxRadius=int(max_rad))
 
-	circles = np.uint16(np.around(circles))
-	for i in circles[0,0:1]:
-	    # draw the outer circle
-	    cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
-	    # draw the center of the circle
-	    cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
+		while circles is None:
+			threshold = max(10, threshold - 10)
+			if threshold == 10:
+				min_rad -= 5
+				max_rad += 5
+			circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,20,
+			                            param1=threshold,param2=40,minRadius=int(min_rad),maxRadius=int(max_rad))	
 
-	centerX = circles[0,0][0];
-	centerY = circles[0,0][1];
-	radius = circles[0,0][2];
+		circles = np.uint16(np.around(circles))
+		min_diff = float('inf')
+		circ = circles[0,0]
+		if not mean_radius:
+			mean_radius = circles[0,0][2]
+		else:
+			for ci in circles[0,:]:
+				if abs(mean_radius - int(ci[2])) < min_diff:
+					circ = ci
+					min_diff = abs(mean_radius - int(ci[2])) 
+			mean_radius = (mean_radius * idx + circ[2]) / (idx + 1)
 
-	if radius > max_radius:
-		max_radius = radius
+		radius = circ[2];
+		stored_circles.append(circ)
 
-	if idx != 0:
-		prevY += PADDING
-		prevX += PADDING
-		template = stored_imgs[idx - 1][prevY - max_radius : prevY + max_radius, prevX - max_radius : prevX + max_radius]
-		centerY, centerX = getBest(border, template, centerY, centerX, max_radius);
-
-	circles[0,0][0] = centerX;
-	circles[0,0][1] = centerY;
-	
-	stored_circles.append(circles[0,0])
-	
-	prevX = centerX;
-	prevY = centerY;
-
-	#cv2.imshow('detected circles',cimg)
-	#cv2.waitKey(0)
-
-max_radius = int(max_radius) + 50;
+		if radius > max_radius:
+			max_radius = radius
 
 
-for i in range(len(stored_imgs)):
-	x = stored_circles[i][0]+PADDING;
-	y = stored_circles[i][1]+PADDING;
-	adjusted_imgs.append(stored_imgs[i][y-max_radius:y+max_radius, x-max_radius:x+max_radius])
-	print(adjusted_imgs[i].shape)
+max_radius = int(max_radius)+50;
 
-# for im in adjusted_imgs:
-# 	cv2.imshow('adjusted',im)
-# 	cv2.waitKey(0)
 
-width , height, channels = adjusted_imgs[0].shape
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
-video = cv2.VideoWriter('eclipse_with.avi',fourcc,1,(width, height))
+video = cv2.VideoWriter('eclipse.avi',fourcc,1,(2 * max_radius, 2 * max_radius))
 
-for im in adjusted_imgs:
-	# cv2.imshow('detected circles',im)
-	# cv2.waitKey(0)
+with progressbar.ProgressBar(max_value=len(imgs)) as bar:
+	for i, file in enumerate(imgs):
+		bar.update(i);
+		raw_img = cv2.imread(file,cv2.IMREAD_COLOR)
+		padded=cv2.copyMakeBorder(raw_img, top=PADDING, bottom=PADDING, left=PADDING, right=PADDING, borderType= cv2.BORDER_CONSTANT, value=[0,0,0] )
+		
+		circ = stored_circles[i]
+		# draw the outer circle
+		cv2.circle(padded,(circ[0]+PADDING,circ[1]+PADDING),circ[2],(0,255,0),10)
+		# draw the center of the circle
+		cv2.circle(padded,(circ[0]+PADDING,circ[1]+PADDING),5,(0,0,255),10)
 
-	video.write(im);
+		x = stored_circles[i][0]+PADDING;
+		y = stored_circles[i][1]+PADDING;
+		adjusted_img = padded[y-max_radius:y+max_radius, x-max_radius:x+max_radius]
+		video.write(adjusted_img)
 
 video.release()
 cv2.destroyAllWindows()
