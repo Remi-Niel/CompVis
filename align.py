003b2afbdc0fff28d68336e0ab6ff676b515843f
voldelord <r.f.niel@student.rug.nl>
2019-04-09 12:33:08 +0200

    refactored code, by moving the alignment function to different files. Added optional flags to align.py. Specifically -p --padding -m --marge -u --ulim

--- align.py
+++ align.py
@@ -1,92 +1,107 @@
 import cv2
 import os
 import glob
-import numpy as np
 import progressbar
+from first_frame import first_frame
+from other_frames import other_frames
 from multiprocessing import Pool
-from rot_align import angle_offset
+import getopt, sys
 
-PADDING = 100;
- 
+# read commandline arguments, first
+fullCmdArguments = sys.argv
+# - further arguments
+argumentList = fullCmdArguments[1:]
+
+unixOptions = "p:m:u:"
+gnuOptions = ["padding=", "marge=", "ulim="]  
+
+try:  
+    arguments, values = getopt.getopt(argumentList, unixOptions, gnuOptions)
+except getopt.error as err:  
+    # output error, and return with an error code
+    print (str(err))
+    sys.exit(2)
+
+PADDING = 100	#Padding added around the aligned images
+MARGE = 5		#Error allowed between the radii of circles
+
 imgs = glob.glob('imgs/' +'*.tif',recursive=True)
 imgs.sort()
 
+upper_lim = min(cv2.imread(imgs[0],0).shape[:2]) / 2 #Max radius of celestial body
 
-stored_circles = [];
-target_radius = 0
-upper_lim = 1700
+for arg, val in arguments:
+	if arg in ("-p", "--padding"):
+		PADDING = int(val)
+	elif arg in ("-m", "--marge"):
+		MARGE = int(val)
+	elif arg in ("-u", "--ulim"):
+		upper_lim = int(val)
 
+stored_circles = []
+stored_COM = []
 
-def loopBody(data = [0, 1700]):
-	print("Frame: " + str(data[0]))
-	upper_lim = data[1]
-
-	raw_img = cv2.imread(imgs[data[0]],cv2.IMREAD_COLOR)
-
-	img = cv2.imread(imgs[data[0]],0)
-
-	threshold = 50;
-	min_rad = upper_lim - 5;
-	max_rad = upper_lim;
-	edge_thresh = 40;
-	 
-	circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,20,
-	                            param1=edge_thresh,param2=edge_thresh,minRadius=int(min_rad),maxRadius=int(max_rad))
-
-	while circles is None:
-		min_rad -= 5
-		max_rad -= 5
-
-		if (min_rad < target_radius - 25):
-			min_rad = upper_lim - 5;
-			max_rad = upper_lim;
-			threshold *= 0.5;
-
-			if threshold < 10:
-				edge_thresh -= 5
-				threshold = 50
-
-		circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,20,
-	                            param1=edge_thresh,param2=edge_thresh,minRadius=int(min_rad),maxRadius=int(max_rad))
-
-	circles = np.uint16(np.around(circles))
-
-	radius = circles[0,0][2];
-
-	return (radius , circles[0,0]);
-
-target_radius, circ = loopBody()
+#Process first frame
+target_radius, circ, com = first_frame([imgs[0], upper_lim, upper_lim / 2])
 
 stored_circles.append(circ)
+stored_COM.append(com)
 
-inputList = [[idx, target_radius] for idx in range(1,len(imgs))]
+#Create input list and process pool
+inputList = [[imgs[idx], target_radius+MARGE,2*MARGE] for idx in range(1,len(imgs))]
 p = Pool(4)
-circles = p.map(loopBody, inputList);
+#Process the other frames in parallel
+results = p.map(other_frames, inputList);
 
-del p
+#clean up process pool
+del p 
 
-stored_circles = stored_circles + [circ[1] for circ in circles]
+stored_circles = stored_circles + [res[1] for res in results]
+stored_COM = stored_COM + [res[2] for res in results]
 
+# make dir, continue if it already exists
+try:  
+    os.mkdir("output_translation_aligned_marked")
+except:
+	pass
 
-target_radius = int(target_radius)+50;
-
-
+#make dir, continue if it already exists
 try:  
     os.mkdir("output_translation_aligned")
 except:
 	pass
 
+#Create aligned images and store unmarked and marked versions to disk.
+#The marked version are rescaled to 800x800
 with progressbar.ProgressBar(max_value=len(imgs)) as bar:
 	for i, file in enumerate(imgs):
 		bar.update(i);
 		raw_img = cv2.imread(file,cv2.IMREAD_COLOR)
-		padded=cv2.copyMakeBorder(raw_img, top=PADDING, bottom=PADDING, left=PADDING, right=PADDING, borderType= cv2.BORDER_CONSTANT, value=[0,0,0] )
+		padded=cv2.copyMakeBorder(raw_img, top=2*PADDING, bottom=2*PADDING, left=2*PADDING, right=2*PADDING, borderType= cv2.BORDER_CONSTANT, value=[0,0,0] )
 		
 		circ = stored_circles[i]
 
-		x = stored_circles[i][0]+PADDING;
-		y = stored_circles[i][1]+PADDING;
-		adjusted_img = padded[y-target_radius:y+target_radius, x-target_radius:x+target_radius]
-		cv2.imwrite("output_translation_aligned/frame_" + str(i) +".png",adjusted_img)
+		x = stored_circles[i][0]+2*PADDING;
+		y = stored_circles[i][1]+2*PADDING;
+		adjusted_img = padded[y-target_radius - PADDING:y+target_radius+PADDING, x-target_radius-PADDING:x+target_radius+PADDING]
+		cv2.imwrite("output_translation_aligned/frame_" + "{:05}".format(i) +".png",adjusted_img)
+
+
+		# draw the outer circle
+		cv2.circle(padded,(circ[0]+2*PADDING,circ[1]+2*PADDING),circ[2],(0,255,0),20)
+		# draw the center of the circle
+		cv2.circle(padded,(circ[0]+2*PADDING,circ[1]+2*PADDING),20,(0,0,255),40)
+
+		com = stored_COM[i]
+		cv2.circle(padded,(int(com[1] + 0.5) + 2*PADDING,int(com[0] + 0.5) + 2*PADDING),20,(255,0,255),40)
+
+		adjusted_img = padded[y-target_radius-PADDING:y+target_radius+PADDING, x-target_radius-PADDING:x+target_radius+PADDING]
+
+		cv2.imwrite("output_translation_aligned_marked/frame_" + "{:05}".format(i) +".png",cv2.resize(adjusted_img, (800, 800)))
+
+
+
+
+
 
 
---
+++ first_frame.py
@@ -0,0 +1,44 @@
+import cv2
+from scipy import ndimage
+import numpy as np
+
+#data is of form [filename, upper radius limit, radius range]
+def first_frame(data):
+	print("Processing: " + data[0])
+
+	upper_lim = data[1]
+	img = cv2.imread(data[0],0)
+
+	threshold = 40;
+	min_rad = upper_lim - 5;
+	max_rad = upper_lim;
+	edge_thresh = 40;
+
+	hist, thresh = cv2.threshold(img,30,255,cv2.THRESH_BINARY)
+
+	com = ndimage.measurements.center_of_mass(thresh)
+
+	circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,20,
+	                            param1=edge_thresh,param2=threshold,minRadius=int(min_rad),maxRadius=int(max_rad))
+
+	while circles is None:
+		min_rad -= 5
+		max_rad -= 5
+
+		if (min_rad < upper_lim - data[2]):
+			min_rad = upper_lim - 5;
+			max_rad = upper_lim;
+			threshold -= 5;
+
+			if threshold < 10:
+				edge_thresh -= 5
+				threshold = 40
+
+		circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,20,
+	                            param1=edge_thresh,param2=threshold,minRadius=int(min_rad),maxRadius=int(max_rad))
+
+	circles = np.uint16(np.around(circles))
+
+	radius = circles[0,0][2];
+
+	return (radius , circles[0,0], com);
